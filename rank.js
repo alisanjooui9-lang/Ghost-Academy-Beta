f16f0890d99f9251fbddaf246a32b11d3c768992
Ghost Academy Support <alisanjooui9@gmail.com>
2026-07-24 10:27:04 +0330

    رنک

---
+++ rank.js
@@ -0,0 +1,33 @@
+function getRank(level){
+
+if(level >= 20){
+
+return "👑 Legendary";
+
+}
+
+
+if(level >= 10){
+
+return "💎 Master";
+
+}
+
+
+if(level >= 5){
+
+return "🥇 Elite";
+
+}
+
+
+if(level >= 3){
+
+return "🥈 Veteran";
+
+}
+
+
+return "🥉 Rookie";
+
+}
